8279254e88a4ab12c428f07dbdf692c2e9fea9a3
ALI-ZIA -KHAN <azk443.443@gmail.com>
2023-03-26 04:01:11 +0500

    watched till 33 pasted from branch 37

--- src/index.js
+++ src/index.js
@@ -4,16 +4,21 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { AppProvider } from "./context/productcontext";
-const root = ReactDOM.createRoot(document.getElementById("root"));
+import { FilterContextProvider } from "./context/filter_context";
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
+//Provider with more bigger scope or parental scope should be higher
 root.render(
-    <AppProvider>
+  <AppProvider>
+    <FilterContextProvider>
       <App />
-    </AppProvider>
-  );
-  
+    </FilterContextProvider>
+  </AppProvider>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
